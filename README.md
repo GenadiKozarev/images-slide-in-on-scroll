3cbe8d5c99d6f08972686e5e2eec9dd2b81290bf
gkoz <genadi.kozarev+github@gmail.com>
2024-11-15 14:42:34 +0000

    calculates and integrates slide-in effect on the images

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Live:
 Preview:
 
 Keywords:
+- debounce()
+- [Window: innerHeight property]('https://developer.mozilla.org/en-US/docs/Web/API/Window/innerHeight')
+- [Window: scrollY property]('https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY')
+- [HTMLElement: offsetTop property]('https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetTop')
+- [Element: scroll event]('https://developer.mozilla.org/en-US/docs/Web/API/Element/scroll_event')
--- script.js
+++ script.js
@@ -21,7 +21,17 @@ const debounce = (func, wait = 20, immediate = true) => {
 };
 
 const checkSlide = e => {
-    console.count(e);
+    sliderImages.forEach(sliderImage => {
+        // half way through the image
+        const slideInAt =
+            window.scrollY + window.innerHeight - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        isHalfShown && isNotScrolledPast
+            ? sliderImage.classList.add('active')
+            : sliderImage.classList.remove('active');
+    });
 };
 
 window.addEventListener('scroll', debounce(checkSlide));
